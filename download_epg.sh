a9f6c90c56bb957984d1e335c0ed880770a08290
Sam Sam <58818070+samleong123@users.noreply.github.com>
2021-09-17 10:15:11 +0800

    Create download_epg.sh

---
+++ download_epg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\e[1;31m [INFO] Downloading Aria2 \e[0m"
+wget https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz
+
+echo -e "\e[1;31m [INFO] Extracting Aria2 \e[0m"
+tar -zxf aria2-1.36.0-static-linux-amd64.tar.gz
+ls -a 
+
+echo -e "\e[1;31m [INFO] Downloading EPG from weareblahs/epg \e[0m"
+rm *.xml
+chmod a+X aria2c
+./aria2c -x 10 -s 10 https://github.com/weareblahs/epg/raw/master/rtmklik.xml
+
+echo -e "\e[1;31m [INFO] Cleaning up \e[0m"
+rm *.tar.gz
+rm aria2c
+
+echo -e "\e[1;31m [INFO] Done \e[0m"
